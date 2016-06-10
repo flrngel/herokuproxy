d8374838e701ca98bc997f14b553eef3afc3c118
flrngel <flrngel@gmail.com>
2016-06-11 01:18:06 +0900

    reverseIP

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 var http = require('http'),
     httpProxy = require('http-proxy');
-httpProxy.createProxyServer({target:'http://59.5.181.247:9999'}).listen(process.env.PORT);
+httpProxy.createProxyServer({target: `http://${process.env.REVERSEIP}:9999`}).listen(process.env.PORT);
