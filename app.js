63348182586a5cc2ae760f58caf760419f13db3c
flrngel <flrngel@gmail.com>
2016-10-11 02:33:28 +0900

    add error tolerating

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 var http = require('http'),
     httpProxy = require('http-proxy');
-httpProxy.createProxyServer({target: `http://${process.env.REVERSEIP}:9999`}).listen(process.env.PORT);
+var proxy = httpProxy.createProxyServer({target: `http://${process.env.REVERSEIP}:9999`})
+proxy.listen(process.env.PORT);
+proxy.on('error', function(err, req, res) {
+  console.log(err);
+});
