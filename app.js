50e0a9ccf0bab641695d2f50f69bcf7548dee8d9
flrngel <flrngel@gmail.com>
2016-10-11 02:49:20 +0900

    add reverseport

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http'),
     httpProxy = require('http-proxy');
-var proxy = httpProxy.createProxyServer({target: `http://${process.env.REVERSEIP}:9999`})
+var proxy = httpProxy.createProxyServer({target: `http://${process.env.REVERSEIP}:${process.env.REVERSEPORT||"9999"}`})
 proxy.listen(process.env.PORT);
 proxy.on('error', function(err, req, res) {
   console.log(err);
